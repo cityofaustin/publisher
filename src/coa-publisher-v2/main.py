2d6fd1319c3f2005a54c36bf5d85e2fcd1c11db9
Niicck <niicck@users.noreply.github.com>
2020-03-16 21:00:35 -0500

    remove old publisher v2 attempt

--- src/coa-publisher-v2/main.py
+++
@@ -1,9 +0,0 @@
-import os, sys
-sys.path.append(os.path.join(os.path.dirname(__file__), './src'))
-
-from app import app
-
-# Only needed for local development
-# Zappa handles the "app" object directly
-if __name__ == '__main__':
-    app.run()
